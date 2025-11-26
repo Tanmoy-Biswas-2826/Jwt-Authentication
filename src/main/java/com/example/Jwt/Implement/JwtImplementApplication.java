2d46a2ef61d5ec05dd8fea7d062f6575b789e4f5
Tanmoy-Biswas-2826 <tanmoybiswas2826@gmail.com>
2025-11-26 15:25:16 +0530

    initial commit

---
+++ src/main/java/com/example/Jwt/Implement/JwtImplementApplication.java
@@ -0,0 +1,14 @@
+package com.example.Jwt.Implement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class JwtImplementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtImplementApplication.class, args);
+		System.out.println("Run Application");
+
+	}
+
+}
